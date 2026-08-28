b4a889812517bd850457d2bd217fcf91e33412c1
Kensarr <ceokensar@gmail.com>
2026-08-29 02:41:20 +0700

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
 // Render Lucide Icons
 lucide.createIcons();
-
-// Smooth Scroll untuk Menu Navigasi
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
-    });
-});
