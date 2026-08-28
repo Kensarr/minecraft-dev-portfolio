5ee0c04c913c862866c1926c7943e84cdc17d694
Kensarr <ceokensar@gmail.com>
2026-08-29 02:27:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Render Lucide Icons
+lucide.createIcons();
+
+// Smooth Scroll untuk Menu Navigasi
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
